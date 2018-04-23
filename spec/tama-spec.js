ffe256f322d5a3203cb2f962f8b9ab0a94daf540
Bobby Martin and Pablo Barreyro <bobby@>
2018-04-23 10:53:03 -0700

    add failing tests for pet death by hunger

--- spec/tama-spec.js
+++ spec/tama-spec.js
@@ -2,7 +2,26 @@ import { pet } from './../src/tama-pet.js';
 
 describe('TamaPet', function() {
   let testPet = pet;
-  it('should create a hungry pet', function() {
+
+  beforeEach(function() {
+    jasmine.clock().install();
+    testPet.hunger();
+  });
+
+  afterEach(function() {
+    jasmine.clock().uninstall();
+    testPet.hunger();
+  });
+
+  it('should create an initial pet', function() {
+    expect(testPet.tummy).toEqual(10);
+  });
+  it('should die', function() {
+    jasmine.clock().tick(10001);
     expect(testPet.tummy).toEqual(0);
+  });
+  it('should die and sting is returned', function() {
+    jasmine.clock().tick(10001);
+    expect(testPet.petCemetery()).toEqual(true);
   });
 });
