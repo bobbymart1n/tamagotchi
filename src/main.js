e9f8d739044c4a896908809a9e8437464fe1dd69
Bobby Martin and Pablo Barreyro <bobby@>
2018-04-23 16:33:57 -0700

    add user logic with Giphy api call

--- src/main.js
+++ src/main.js
@@ -1,4 +1,26 @@
-// import $ from 'jquery';
+import { pet } from './tama-pet.js';
+import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
+
+$(function() {
+  $("#startPet").submit(function(event) {
+    event.preventDefault();
+    let newPet = pet;
+    $(this).hide();
+    $(".pet-container").show();
+    $("#age span").text(newPet.age);
+    $.ajax({
+      url: `http://api.giphy.com/v1/gifs/CjrB9Mo0o4DNC?api_key=${process.env.API_KEY}`,
+      type: 'GET',
+      data: {
+        format: 'json'
+      },
+      success: function(res) {
+        console.log(res);
+        $("#playfullness").append(`<img src=${res.data.url}>`)
+      }
+    });
+  });
+});
